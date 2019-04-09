297215f6bc4604eb1c004ac5b9ef9f5b3eba4fd8
Jakub Konka <kubkon@jakubkonka.com>
2019-04-09 08:25:07 +0200

    Add better handling of error messages

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,21 @@ use std::env;
 use std::fs;
 use std::io::{Read, Write};
 
+fn process(input_fname: &str, output_fname: &str) -> Result<(), String> {
+    let mut input_file =
+        fs::File::open(input_fname).map_err(|err| format!("error opening input: {}", err))?;
+    let mut contents = Vec::new();
+    input_file
+        .read_to_end(&mut contents)
+        .map_err(|err| format!("read error: {}", err))?;
+
+    let mut output_file = fs::File::create(output_fname)
+        .map_err(|err| format!("error opening output '{}': {}", output_fname, err))?;
+    output_file
+        .write_all(&contents)
+        .map_err(|err| format!("write error: {}", err))
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let program = args[0].clone();
@@ -10,17 +25,8 @@ fn main() {
         eprintln!("{} <input_file> <output_file>", program);
         return;
     }
-    let input_fname = &args[1];
-    let output_fname = &args[2];
 
-    let mut input_file = fs::File::open(input_fname)
-        .unwrap_or_else(|err| panic!("error opening input {}: {}", input_fname, err));
-    let mut contents = Vec::new();
-    input_file.read_to_end(&mut contents)
-        .unwrap_or_else(|err| panic!("read error: {}", err));
-
-    let mut output_file = fs::File::create(output_fname)
-        .unwrap_or_else(|err| panic!("error opening output {}: {}", output_fname, err));
-    output_file.write_all(&contents)
-        .unwrap_or_else(|err| panic!("write error: {}", err));
+    if let Err(err) = process(&args[1], &args[2]) {
+        eprintln!("{}", err)
+    }
 }
